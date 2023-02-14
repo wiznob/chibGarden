24e1439cfcfb7b67afa58cbad3f43b8c1a355fbe
wiz <wisbob27@gmail.com>
2023-02-14 11:15:06 +0000

    moved fonts

--- stamina.gd
+++ stamina.gd
@@ -8,7 +8,7 @@ extends ProgressBar
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Tween
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

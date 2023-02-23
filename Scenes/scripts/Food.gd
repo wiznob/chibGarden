b43bc0150082afd20d740501529b43a4e4e1be8e
wiz <wisbob27@gmail.com>
2023-02-23 16:57:37 +0000

    Food summon

--- Scenes/scripts/Food.gd
+++ Scenes/scripts/Food.gd
@@ -4,15 +4,12 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var a
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-	a = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	print("Test", a)
-	a = a+1
+#func _process(delta):
+
--- Scenes/scripts/Menu.gd
+++ Scenes/scripts/Menu.gd
@@ -3,6 +3,8 @@ extends Control
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var food = load("res://Scenes/Food.tscn")
+var newFood = food.instance()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,6 +16,8 @@ func _on_settingsBTN_pressed():
 	 else:
 			get_tree().paused = false
 
+func _on_feedBTN_pressed():
+	get_parent().add_child(newFood)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):

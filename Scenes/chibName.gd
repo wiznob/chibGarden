7f6dd8d1154041fb6196b8cd5a5c1d4ee3cdc994
wiz <wisbob27@gmail.com>
2023-02-25 18:54:44 +0000

    Chib name display on menu scene

--- Scenes/chibName.gd
+++ Scenes/chibName.gd
@@ -1,15 +1,15 @@
-extends RichTextLabel
+extends "res://Scenes/scripts/RichTextLabel.gd"
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var names = "This is a chib" 
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	add_text(names)
-	hide()
+	show()
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):

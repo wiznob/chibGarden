765046d991661150bbda7883391937f54c17417b
wiz <wisbob27@gmail.com>
2023-04-05 21:14:47 +0100

    Stats can no longer go lower than 0

--- Scenes/scripts/MiniGame_Script/MiniGamePop.gd
+++ Scenes/scripts/MiniGame_Script/MiniGamePop.gd
@@ -1,14 +1,28 @@
 extends Button
-
+onready var save_file = SaveData.gameData
 
 
 func _on_ChibSearch_pressed():
-	get_tree().change_scene("res://Scenes/minigame/Level1.tscn")
 	get_tree().paused = false
+	var decrease = save_file.stamina - 10
+	save_file.stamina = decrease
+	if(save_file.stamina < 0):
+		var reset = 0
+		save_file.stamina = reset
+		SaveData.save_data()
+	SaveData.save_data()
+	get_tree().change_scene("res://Scenes/minigame/Level1.tscn")
 	
 
 
 func _on_ChibRacing_pressed():
-	get_tree().change_scene("res://Scenes/minigame2/EndGame.tscn")
 	get_tree().paused = false
+	var decrease = save_file.stamina - 10
+	save_file.stamina = decrease
+	if(save_file.stamina < 0):
+		var reset = 0
+		save_file.stamina = reset
+		SaveData.save_data()
+	get_tree().change_scene("res://Scenes/minigame2/EndGame.tscn")
+	
 	
